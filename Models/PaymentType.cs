ceb993d10aea253b4bc07e5b0c7f4f845679865c
Josh Hibray <jjhibray@gmail.com>
2019-06-25 10:35:35 -0500

    finished models for project

---
+++ Models/PaymentType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class PaymentType
+    {
+        public int Id { get; set; }
+
+        [Required]
+        private int AccountNumber { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        public void addAccountNumber(int num)
+        {
+            AccountNumber = num;
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ProductTypeId { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        public List<Order> ProductsOrdered { get; set; } = new List<Order>();
+
+
+    }
+}
