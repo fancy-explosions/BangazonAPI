835e9afc7b6207709a6997423e80ea138daff876
anneraev <47757104+anneraev@users.noreply.github.com>
2019-06-27 15:12:03 -0500

    test

---
+++ TestBangazonAPI/TestDepartments.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+using Xunit;
+using BangazonAPI.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace TestBangazonAPI
+{
+    public class TestDepartments
+    {
+        [Fact]
+        public async Task Test_Get_All_Departments()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+
+                /*
+                    ACT
+                */
+                var response = await client.GetAsync("/api/Departments");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var customers = JsonConvert.DeserializeObject<List<Customer>>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(customers.Count > 0);
+            }
+        }
+    }
+}
